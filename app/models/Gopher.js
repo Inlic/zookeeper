3daafaee574dd49a0596e7d8d0e8ce8e0e53430c
Ben W <bwiedeback@gmail.com>
2020-08-24 14:50:11 -0600

    updates to files new classes

---
+++ app/models/Gopher.js
@@ -0,0 +1,27 @@
+class Gopher {
+  constructor(
+    name,
+    color = "brown",
+    gender,
+    weight,
+    tail = true,
+    covering = "skin",
+    diet = "omnivore",
+    wouldFight = "Yes"
+  ){
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.weight = weight;
+    this.tail = tail;
+    this.covering = covering;
+    this.diet = diet;
+    this.wouldFight = wouldFight;
+  }
+  eats(){
+    return `${this.name} is a ${this.diet}.`
+  }
+  fight(){
+    return `Would I fight ${this.name} with my bare hands? ${this.wouldFight}.`
+  }
+}
